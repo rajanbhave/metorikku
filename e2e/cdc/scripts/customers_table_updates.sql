f525d095a4f30d0dc9bcc538bb9813df88810898
Ofir Ventura <oventura@yotpo.com>
2019-04-24 12:26:13 +0300

    feat(hudi-e2e): added test case for delete in hudi cdc e2e

--- e2e/cdc/scripts/customers_table_updates.sql
+++ e2e/cdc/scripts/customers_table_updates.sql
@@ -4,4 +4,6 @@ UPDATE customers SET first_name='Anne Mariesss' WHERE id=1004;
 UPDATE customers SET first_name='Anne Mariessss' WHERE id=1004;
 UPDATE customers SET first_name='Anne Mariesssss' WHERE id=1004;
 
+DELETE FROM customers WHERE id=1003;
+
 SELECT * FROM customers;
